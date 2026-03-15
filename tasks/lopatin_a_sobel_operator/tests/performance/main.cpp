58b8ef1227b0c68405ba814ab599a30a95dd6364
vinch <a478honor0109@gmail.com>
2026-03-15 19:53:11 +0300

    feat(omp): add omp impl

--- tasks/lopatin_a_sobel_operator/tests/performance/main.cpp
+++ tasks/lopatin_a_sobel_operator/tests/performance/main.cpp
@@ -6,6 +6,7 @@
 
 #include "lopatin_a_sobel_operator/common/include/common.hpp"
 #include "lopatin_a_sobel_operator/seq/include/ops_seq.hpp"
+#include "lopatin_a_sobel_operator/omp/include/ops_omp.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace lopatin_a_sobel_operator {
@@ -15,8 +16,8 @@ class LopatinARunPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType>
   OutType output_chekup_data_;
 
   void SetUp() override {
-    std::size_t height = 3840;
-    std::size_t width = 2160;
+    std::size_t height = 7680;
+    std::size_t width = 4320;
 
     input_data_.height = height;
     input_data_.width = width;
@@ -50,7 +51,7 @@ TEST_P(LopatinARunPerfTests, RunPerfModes) {
 namespace {
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, LopatinASobelOperatorSEQ>(PPC_SETTINGS_lopatin_a_sobel_operator);
+    ppc::util::MakeAllPerfTasks<InType, LopatinASobelOperatorSEQ, LopatinASobelOperatorOMP>(PPC_SETTINGS_lopatin_a_sobel_operator);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
