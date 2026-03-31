94750a11bdc2dce2f3a14e732cc422b189936a23
TIMUR <timur.sinev2000@mail.ru>
2026-04-01 00:09:45 +0300

    stl1.06

--- tasks/timur_a_cannon/stl/src/ops_stl.cpp
+++ tasks/timur_a_cannon/stl/src/ops_stl.cpp
@@ -1,12 +1,91 @@
 #include "timur_a_cannon/stl/include/ops_stl.hpp"
 
-#include <cmath>
+#include <algorithm>
+#include <cstddef>
 #include <thread>
+#include <utility>
 #include <vector>
 
 #include "util/include/util.hpp"
 
 namespace timur_a_cannon {
+
+namespace {
+
+using Matrix = std::vector<std::vector<double>>;
+using BlockGrid = std::vector<std::vector<Matrix>>;
+
+template <typename Func>
+void ParallelFor(int work_size, Func &&func) {
+  if (work_size <= 0) {
+    return;
+  }
+
+  const int num_threads = std::max(1, std::min(ppc::util::GetNumThreads(), work_size));
+  std::vector<std::thread> threads;
+  threads.reserve(static_cast<std::size_t>(num_threads));
+
+  for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+    threads.emplace_back([&, thread_id]() {
+      for (int index = thread_id; index < work_size; index += num_threads) {
+        func(index);
+      }
+    });
+  }
+
+  for (auto &thread : threads) {
+    thread.join();
+  }
+}
+
+void DistributeData(const Matrix &src_a, const Matrix &src_b, BlockGrid &bl_a, BlockGrid &bl_b, int b_size,
+                    int grid_sz) {
+  ParallelFor(grid_sz, [&](int i) {
+    for (int j = 0; j < grid_sz; ++j) {
+      const int shift = (i + j) % grid_sz;
+      for (int row = 0; row < b_size; ++row) {
+        for (int col = 0; col < b_size; ++col) {
+          bl_a[i][j][row][col] = src_a[(i * b_size) + row][(shift * b_size) + col];
+          bl_b[i][j][row][col] = src_b[(shift * b_size) + row][(j * b_size) + col];
+        }
+      }
+    }
+  });
+}
+
+void RotateBlocksA(BlockGrid &blocks, int grid_sz) {
+  ParallelFor(grid_sz, [&](int i) {
+    Matrix first_block = std::move(blocks[i][0]);
+    for (int j = 0; j < grid_sz - 1; ++j) {
+      blocks[i][j] = std::move(blocks[i][j + 1]);
+    }
+    blocks[i][grid_sz - 1] = std::move(first_block);
+  });
+}
+
+void RotateBlocksB(BlockGrid &blocks, int grid_sz) {
+  ParallelFor(grid_sz, [&](int j) {
+    Matrix first_block = std::move(blocks[0][j]);
+    for (int i = 0; i < grid_sz - 1; ++i) {
+      blocks[i][j] = std::move(blocks[i + 1][j]);
+    }
+    blocks[grid_sz - 1][j] = std::move(first_block);
+  });
+}
+
+void CollectResult(const BlockGrid &bl_c, Matrix &result, int b_size, int grid_sz) {
+  ParallelFor(grid_sz, [&](int i) {
+    for (int j = 0; j < grid_sz; ++j) {
+      for (int row = 0; row < b_size; ++row) {
+        for (int col = 0; col < b_size; ++col) {
+          result[(i * b_size) + row][(j * b_size) + col] = bl_c[i][j][row][col];
+        }
+      }
+    }
+  });
+}
+
+}  // namespace
 
 TimurACannonMatrixMultiplicationSTL::TimurACannonMatrixMultiplicationSTL(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
@@ -22,8 +101,17 @@ bool TimurACannonMatrixMultiplicationSTL::ValidationImpl() {
   if (b_size <= 0 || mat_a.empty() || mat_b.empty()) {
     return false;
   }
-  size_t n = mat_a.size();
-  return (n == mat_a[0].size() && n == mat_b.size() && n == mat_b[0].size() && (n % b_size == 0));
+
+  const std::size_t n = mat_a.size();
+  if (mat_b.size() != n || (n % static_cast<std::size_t>(b_size) != 0)) {
+    return false;
+  }
+
+  const auto is_square_n = [n](const Matrix &matrix) {
+    return std::all_of(matrix.begin(), matrix.end(), [n](const std::vector<double> &row) { return row.size() == n; });
+  };
+
+  return is_square_n(mat_a) && is_square_n(mat_b);
 }
 
 bool TimurACannonMatrixMultiplicationSTL::PreProcessingImpl() {
@@ -46,86 +134,35 @@ void TimurACannonMatrixMultiplicationSTL::BlockMultiplyAccumulate(const std::vec
 
 bool TimurACannonMatrixMultiplicationSTL::RunImpl() {
   const auto &input = GetInput();
-  int b_size = std::get<0>(input);
+  const int b_size = std::get<0>(input);
   const auto &src_a = std::get<1>(input);
   const auto &src_b = std::get<2>(input);
-  int n = static_cast<int>(src_a.size());
-  int grid_sz = n / b_size;
-
-  using Matrix = std::vector<std::vector<double>>;
-  using BlockGrid = std::vector<std::vector<Matrix>>;
+  const int n = static_cast<int>(src_a.size());
+  const int grid_sz = n / b_size;
 
   BlockGrid bl_a(grid_sz, std::vector<Matrix>(grid_sz, Matrix(b_size, std::vector<double>(b_size))));
   BlockGrid bl_b(grid_sz, std::vector<Matrix>(grid_sz, Matrix(b_size, std::vector<double>(b_size))));
   BlockGrid bl_c(grid_sz, std::vector<Matrix>(grid_sz, Matrix(b_size, std::vector<double>(b_size, 0.0))));
 
-  int num_threads = ppc::util::GetNumThreads();
-
-  auto parallel_grid_op = [&](auto func) {
-    std::vector<std::thread> threads;
-    for (int t = 0; t < num_threads; ++t) {
-      threads.emplace_back([&, t]() {
-        for (int i = t; i < grid_sz; i += num_threads) {
-          for (int j = 0; j < grid_sz; ++j) {
-            func(i, j);
-          }
-        }
-      });
-    }
-    for (auto &th : threads) {
-      th.join();
-    }
-  };
-
-  parallel_grid_op([&](int i, int j) {
-    int shift = (i + j) % grid_sz;
-    for (int r = 0; r < b_size; ++r) {
-      for (int c = 0; c < b_size; ++c) {
-        bl_a[i][j][r][c] = src_a[i * b_size + r][shift * b_size + c];
-        bl_b[i][j][r][c] = src_b[shift * b_size + r][j * b_size + c];
-      }
-    }
-  });
+  DistributeData(src_a, src_b, bl_a, bl_b, b_size, grid_sz);
 
   for (int step = 0; step < grid_sz; ++step) {
-    parallel_grid_op([&](int i, int j) { BlockMultiplyAccumulate(bl_a[i][j], bl_b[i][j], bl_c[i][j], b_size); });
+    ParallelFor(grid_sz, [&](int i) {
+      for (int j = 0; j < grid_sz; ++j) {
+        BlockMultiplyAccumulate(bl_a[i][j], bl_b[i][j], bl_c[i][j], b_size);
+      }
+    });
 
     if (step < grid_sz - 1) {
-      std::vector<std::thread> shift_threads;
-      shift_threads.emplace_back([&]() {
-        for (int i = 0; i < grid_sz; ++i) {
-          auto first = std::move(bl_a[i][0]);
-          for (int j = 0; j < grid_sz - 1; ++j) {
-            bl_a[i][j] = std::move(bl_a[i][j + 1]);
-          }
-          bl_a[i][grid_sz - 1] = std::move(first);
-        }
-      });
-      shift_threads.emplace_back([&]() {
-        for (int j = 0; j < grid_sz; ++j) {
-          auto first = std::move(bl_b[0][j]);
-          for (int i = 0; i < grid_sz - 1; ++i) {
-            bl_b[i][j] = std::move(bl_b[i + 1][j]);
-          }
-          bl_b[grid_sz - 1][j] = std::move(first);
-        }
-      });
-      for (auto &th : shift_threads) {
-        th.join();
-      }
+      RotateBlocksA(bl_a, grid_sz);
+      RotateBlocksB(bl_b, grid_sz);
     }
   }
 
-  Matrix res_mat(n, std::vector<double>(n));
-  parallel_grid_op([&](int i, int j) {
-    for (int r = 0; r < b_size; ++r) {
-      for (int c = 0; c < b_size; ++c) {
-        res_mat[i * b_size + r][j * b_size + c] = bl_c[i][j][r][c];
-      }
-    }
-  });
+  Matrix result(n, std::vector<double>(n));
+  CollectResult(bl_c, result, b_size, grid_sz);
 
-  GetOutput() = std::move(res_mat);
+  GetOutput() = std::move(result);
   return true;
 }
 
